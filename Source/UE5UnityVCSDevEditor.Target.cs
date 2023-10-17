cd4c57591a5556c470fc21376ccd2ccedbf0040e
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2023-10-17 10:26:13 +0200

    bUseUnityBuild = false;

--- Source/UE5UnityVCSDevEditor.Target.cs
+++ Source/UE5UnityVCSDevEditor.Target.cs
@@ -10,5 +10,9 @@ public class UE5UnityVCSDevEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "UE5UnityVCSDev" } );
+		
+		// Uncomment to rebuild the whole project without Unity Build, compiling each cpp source file individually, in order to test Includ Whay You Use (IWYU) policy
+		// WARNING: don't uncomment on UnrealEngine Source Build, else you will trigger a full new Engine build (but easy to revert, will just relink 1200 lib/dll)
+		bUseUnityBuild = false;
 	}
 }
